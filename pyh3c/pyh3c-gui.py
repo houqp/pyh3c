5faef7892b79d1cec14ab0ad0f1a627e5ff8b57f
Qingping Hou <dave2008713@gmail.com>
2011-09-06 17:19:40 +0800

    finished read config part.
    Now user can edit and save configuration in the GUI.
    Moving on to code the authentication logic.

--- pyh3c/pyh3c-gui.py
+++ pyh3c/pyh3c-gui.py
@@ -3,12 +3,13 @@
 import pygtk
 pygtk.require('2.0')
 import gtk
+import glib
 
 import pcap
 import dpkt
 import binascii
 import dnet
-import commands
+import subprocess
 import threading
 from os import getuid
 
@@ -29,9 +30,10 @@ def create_Frame(label=None):
   frame.show()
   return frame
 
-def create_MenuItem(name, callback, data=None):
+def create_MenuItem(name, callback=None, data=None):
   item = gtk.MenuItem(name)
-  item.connect("activate", callback, data)
+  if callback:
+    item.connect("activate", callback, data)
   item.show()
   return item
 
@@ -40,6 +42,11 @@ def create_OptionMenu(menu):
   opt.set_menu(menu)
   opt.show()
   return opt
+
+def create_MenuBar():
+  menu_bar = gtk.MenuBar()
+  menu_bar.show()
+  return menu_bar
 
 def create_Label(str):
   label = gtk.Label(str)
@@ -99,21 +106,23 @@ def show_dialog(title, str):
   dialog.show()
 
 #--- [callback function] ---
-def change_eth(item, eth):
-  print "Changed to " + eth
-
 
 class H3C_GUI:
   def __init__(self):
+
+    self.pyh3c = PyH3C()
+
     self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
     self.window.show()
 
     #register signals with handlers
     self.window.connect("delete_event", self.delete_event)
     self.window.connect("destroy", self.destroy) 
-    self.window.set_border_width(20)
+    #self.window.set_border_width(20)
 
     #setup boxes
+    box_root = create_VBox(False, 0)
+
     box_main = create_VBox(False, 0)
     box_main.set_border_width(10)
 
@@ -130,25 +139,46 @@ class H3C_GUI:
     label_pass = create_Label("Passward:")
     label_eth = create_Label("Interfaces:")
     label_dhcp = create_Label("DHCP scripts:")
+    label_ping_target = create_Label("Ping target:")
     #endof labels
 
     #setup entry
-    self.entry_user = create_Entry(0, "houqp")
+    self.entry_user = create_Entry(0, None)
+    self.entry_user.connect("changed", self.user_name_changed)
     self.entry_pass = create_Entry(0, None)
     self.entry_pass.set_visibility(gtk.FALSE)
+    self.entry_pass.connect("changed", self.user_pass_changed)
     self.entry_dhcp = create_Entry(0, None)
+    self.entry_dhcp.connect("changed", self.dhcp_command_changed)
+    self.entry_ping_target = create_Entry(0, None)
+    self.entry_ping_target.connect("changed", self.ping_target_changed)
     #endof entry
+
+    #setup root_menu
+    root_menu_bar = create_MenuBar()
+
+    file_menu = gtk.Menu()
+    file_item = create_MenuItem("File")
+    quit_item = create_MenuItem("Quit", self.destroy)
+
+    help_menu = gtk.Menu()
+    help_item = create_MenuItem("Help")
+    about_item = create_MenuItem("About", self.hello)
+    #endof root_menu
   
     #setup OptionMenus
-    menu_eth = gtk.Menu()
-    item = create_MenuItem("eth0", change_eth, "eth0")
-    menu_eth.append(item)
-    item = create_MenuItem("wlan0", change_eth, "wlan0")
-    menu_eth.append(item)
-    opt_eth = create_OptionMenu(menu_eth)
+    menu_dev = gtk.Menu()
+    for dev in self.pyh3c.get_devices():
+      if dev != "lo":
+        item = create_MenuItem(dev, self.change_dev, dev)
+        menu_dev.append(item)
+    opt_dev = create_OptionMenu(menu_dev)
     #endof OptionMenus
 
     #setup buttons
+    button_save_config = create_Button("Save")
+    button_save_config.connect("clicked", self.save_config, None)
+
     button_connect = create_Button("Connect")
     button_connect.connect("clicked", self.pressed_connect, None)
 
@@ -158,11 +188,8 @@ class H3C_GUI:
     button_hide = create_Button("Hide me!")
     button_hide.connect("clicked", self.pressed_hide, None)
 
-    button_exit = create_Button("Quit", gtk.STOCK_QUIT)
-    button_exit.connect_object("clicked", gtk.Widget.destroy, self.window)
-
-    button_about = create_Button("About", gtk.STOCK_DIALOG_INFO)
-    button_about.connect("clicked", self.hello, None)
+    button_quit = create_Button("Quit", gtk.STOCK_QUIT)
+    button_quit.connect("clicked", lambda a,b: gtk.main_quit(), None)
     #endup buttons
 
     #setup frames
@@ -172,10 +199,6 @@ class H3C_GUI:
 
     #setup TextView
     welcome_text = """
-[*] Activities from server.
-[#] Activities from client.
-[!] Messages you may want to read.
-
 """
     textview_status = create_TextView(welcome_text)
     textview_sw_status = create_ScrolledWindow()
@@ -190,13 +213,26 @@ class H3C_GUI:
 
 
     #start to pack widgets
-    self.window.add(box_main)
+    self.window.add(box_root)
+    #-----------------------------
+    box_root.pack_start(root_menu_bar, False, False, 0)
+    box_root.pack_start(box_main, False, False, 0)
     #-----------------------------
     box_main.pack_start(frame_config, False, False, 10)
     box_main.pack_start(frame_status, True, True, 10)
     box_main.pack_start(hsep_status, False, False, 10)
     box_main.pack_start(box_buttons, False, False, 10)
     #-----------------------------
+    # start of menu bar
+    root_menu_bar.append(file_item)
+    file_item.set_submenu(file_menu)
+    root_menu_bar.append(help_item)
+    help_item.set_submenu(help_menu)
+
+    file_menu.append(quit_item)
+    help_menu.append(about_item)
+
+    # start of table
     frame_config.add(box_config)
     box_config.pack_start(table_config, False, False, 10)
     #   0       1       2
@@ -216,20 +252,66 @@ class H3C_GUI:
     table_config.attach(self.entry_pass, 1, 2, 1, 2, gtk.FILL, gtk.FILL, 0, 0)
 
     table_config.attach(label_eth, 0, 1, 2, 3, gtk.FILL, gtk.FILL, 0, 0)
-    table_config.attach(opt_eth, 1, 2, 2, 3, gtk.FILL, gtk.FILL, 0, 0)
+    table_config.attach(opt_dev, 1, 2, 2, 3, gtk.FILL, gtk.FILL, 0, 0)
 
     table_config.attach(label_dhcp, 0, 1, 3, 4, gtk.FILL, gtk.FILL, 0, 0)
     table_config.attach(self.entry_dhcp, 1, 2, 3, 4, gtk.FILL, gtk.FILL, 0, 0)
+
+    table_config.attach(label_ping_target, 0, 1, 4, 5, gtk.FILL, gtk.FILL, 0, 0)
+    table_config.attach(self.entry_ping_target, 1, 2, 4, 5, gtk.FILL, gtk.FILL, 0, 0)
+    
+    box_config.pack_start(button_save_config, True, False, 5)
     #box_config.pack_start(entry_user, False, False, 0)
 
+    # start of status info
     frame_status.add(textview_sw_status)
 
+    # start of buttons
     box_buttons.pack_start(button_connect, False, False, 5)
     box_buttons.pack_start(button_disconnect, False, False, 5)
     box_buttons.pack_start(button_hide, False, False, 5)
-    box_buttons.pack_end(button_about, False, False, 5)
-    box_buttons.pack_end(button_exit, False, False, 5)
+    box_buttons.pack_end(button_quit, False, False, 5)
     #endof packing
+
+    #do some real initialization here
+
+    #check whether the file exits before loading
+    try:
+      fp = open('pyh3c.conf')
+    except IOError:
+      pass
+    else:
+      self.pyh3c.h3cStatus.load_config()
+
+    self.entry_user.set_text(self.pyh3c.h3cStatus.user_name)
+    self.entry_pass.set_text(self.pyh3c.h3cStatus.user_pass)
+    self.entry_dhcp.set_text(self.pyh3c.h3cStatus.dhcp_command)
+    self.entry_ping_target.set_text(self.pyh3c.h3cStatus.ping_target)
+
+    try:
+      opt_dev.set_history(
+        self.pyh3c.get_devices().index(self.pyh3c.h3cStatus.dev) - 1
+        )
+    except ValueError:
+      pass
+
+    #self.pyh3c_instance = subprocess.Popen(
+        #["python", "-u", "pyh3c.py", "-k"], 
+        #stdout = subprocess.PIPE        
+        #)
+    self.pyh3c_instance = subprocess.Popen(
+        ["python", "-u", "test.py"], 
+        stdout = subprocess.PIPE        
+        )
+    self.status_output = self.STATUS_OUTPUT(self)
+
+    #passer = threading.Thread(group=None, target=self.pyh3c_output_passer, name='passer', args=(self.status_output,), kwargs={})
+    #passer.daemon = True
+    #passer.start()
+
+    glib.timeout_add_seconds(1, self.pyh3c_output_passer, )
+
+    #self.pyh3c_output_passer()
 
   def hello(self, widget, data=None):
     about_messg = """
@@ -238,33 +320,11 @@ the source code at:
 https://github.com/houqp/pyh3c
 Happy hacking :-)
 
-This piece of software may not be working
-as you expected. But if it really works,
-remember to send me a Thank you letter via
-qingping.hou@gmail.com.
-
-OK, I am just kidding. Forget about this.
-
 Author: houqp
 """
     show_dialog("About", about_messg)
 
   def pressed_connect(self, widget, data=None):
-    if not (getuid() == 0):
-      privilege_warn = " You must run with root privilege!"
-      show_dialog("Warnning", privilege_warn)
-      return
-
-    sender = dnet.eth(h3cStatus.dev)
-    client_hwadd = sender.get()
-   
-    def send_start_callback_gui():
-      buf = "[*] Sent out the authentication request."
-      self.buffer_status.insert(self.buffer_status.get_end_iter(), buf)
-
-    send_start(sender, send_start_callback_gui)
-    pcap_thread = threading.Thread(None, pcap_loop, "pcap_thread", h3c_gui)
-    pcap_thread.start()
     return
 
   def pressed_disconnect(self, widget, data=None):
@@ -284,69 +344,57 @@ Author: houqp
   def destroy(self, widget, data=None):
     gtk.main_quit()
 
-  def identity_handler_callback_gui(self):
-    if h3cStatus.auth_success:
-      print " [*] Received server check online request, sent keepalive packet."
-    else:
-      print " [*] Received identity challenge request."
-      print "     [#] Sent identity challenge response."
+  def change_dev(self, item, dev):
+    self.pyh3c.h3cStatus.dev = dev
+    print "Changed to " + self.pyh3c.h3cStatus.dev
 
-  def h3c_unknown_handler_callback(self):
-    print " [*] Received unknown h3c response from server."
+  def user_name_changed(self, entry, data=None):
+    self.pyh3c.h3cStatus.user_name = entry.get_text()
+    return
 
-  def allocated_handler_callback(self):
-    print " [*] Received allocated challenge request."
-    print "     [#] Sent allocated challenge response."
+  def user_pass_changed(self, entry, data=None):
+    self.pyh3c.h3cStatus.user_pass = entry.get_text()
+    return
+  
+  def dhcp_command_changed(self, entry, data=None):
+    self.pyh3c.h3cStatus.dhcp_command = entry.get_text()
+    return
 
-  def success_handler_callback(self):
-    dhcp_command = "%s %s" % (h3cStatus.dhcp_script, h3cStatus.dev)
-    print ""
-    print "  /---------------------------------------------\ "
-    print " | [^_^] Successfully passed the authentication! |"
-    print "  \---------------------------------------------/ "
-    print ""
-    print " [#] running command: %s to get an IP." % dhcp_command
-    print ""
-    (status, output) = commands.getstatusoutput(dhcp_command)
-    print output
-    print ""
-    print " [!] Every thing is done now, happy surfing the Internet." 
-    print " [!] I will send heart beat packets to keep you online." 
+  def ping_target_changed(self, entry, data=None):
+    self.pyh3c.h3cStatus.ping_target = entry.get_text()
+    return
 
-  def failure_handler_callback(self):
-    print " [*] Received authentication failed packet from server."
-    print "     [#] Try to restart the authentication."
-    send_start()
+  def save_config(self, widget, data=None):
+    self.pyh3c.h3cStatus.save_config()
+    return
 
-def pcap_loop(self):
-  show_dialog("Warnning", "hahahaha")
+  class STATUS_OUTPUT:
+    def __init__(self, h3c_gui):
+      self.h3c_gui = h3c_gui
 
-  hw_s = binascii.b2a_hex(client_hwadd)
-  filter_hwadd = "%s:%s:%s:%s:%s:%s" % (hw_s[0:2], hw_s[2:4], hw_s[4:6], hw_s[6:8], hw_s[8:10], hw_s[10:12])
+    def write(self, text):
+      self.h3c_gui.buffer_status.insert(self.h3c_gui.buffer_status.get_end_iter(), text)
 
-  filter = 'ether host %s and ether proto 0x888e' % filter_hwadd
-  pc = pcap.pcap(h3cStatus.dev)
-  pc.setfilter(filter)
+    def writelines(self, lines):
+      for line in lines: 
+        self.write(line)
 
-  for ptime,pdata in pc:
-    ether = dpkt.ethernet.Ethernet(pdata)
-    if ether.dst == client_hwadd:
-      radius = RADIUS_H3C(ether.data)
-      eap = RADIUS_H3C.EAP(radius.data)
-
-      if response_type[eap.code] == 'request':
-        handler = "%s_handler" % eap_type[eap.type]
-      else:
-        handler = "%s_handler" % response_type[eap.code]
-      hander_callback = "%s_callback" % handler
-      globals()[handler](ether, sender, getattr(self, hander_callback), self)
+  def pyh3c_output_passer(self, output=None):
+    #while True:
+      out = self.pyh3c_instance.stdout.readline()
+      #if not out: continue
+      if out:
+        self.status_output.write(out)
+      return True
 
 class H3C_GUI_ICON(gtk.StatusIcon):
   def __init__(self):
     gtk.StatusIcon.__init__(self)
 
 
+
 if __name__ == "__main__":
+
 
   h3c_gui = H3C_GUI()
 
@@ -354,11 +402,5 @@ if __name__ == "__main__":
   h3c_gui_icon.connect("activate", h3c_gui.unhide)
 
 
-  h3cStatus.load_config()
-  h3c_gui.entry_user.set_text(h3cStatus.user_name)
-  h3c_gui.entry_pass.set_text(h3cStatus.user_pass)
-  h3c_gui.entry_dhcp.set_text(h3cStatus.dhcp_script)
-
-  pcap_loop(h3c_gui)
-
   gtk.main()
+  #gtk.main_iteration_do()
