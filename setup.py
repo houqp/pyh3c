5ff54a00114ac128825b587a36cfb5f51ffabc7b
Qingping Hou <dave2008713@gmail.com>
2011-09-14 21:48:28 +0800

    used __version__ form pyh3c.py in setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,10 @@
 # -*- coding: utf-8 -*-
 
 from distutils.core import setup
+import pyh3c.pyh3c
 
 setup(name='pyh3c',
-      version='',
+      version=pyh3c.pyh3c.__version__,
       description='A program for passing h3c authentication in SYSU east campus.',
       author='houqp',
       author_email='qingping.hou@gmail.com',
